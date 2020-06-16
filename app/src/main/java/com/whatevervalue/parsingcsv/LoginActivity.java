b0abbe758632e3440fcb2522c68f3e3215508b47
Alexandru <alexandrumihet23@gmail.com>
2020-06-16 22:26:38 +0300

    11th commit

--- app/src/main/java/com/whatevervalue/parsingcsv/LoginActivity.java
+++ app/src/main/java/com/whatevervalue/parsingcsv/LoginActivity.java
@@ -3,6 +3,9 @@ package com.whatevervalue.parsingcsv;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -17,6 +20,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.whatevervalue.parsingcsv.ui.home.HomeFragment;
 
 public class LoginActivity extends AppCompatActivity {
     EditText mEmail, mPassword;
@@ -65,7 +69,7 @@ public class LoginActivity extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if(task.isSuccessful()){
                             Toast.makeText(LoginActivity.this, "Logged in succesfully", Toast.LENGTH_SHORT ).show();
-                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                            startActivity(new Intent(getApplicationContext(), Meniu.class));
 
                         }else {
                             Toast.makeText(LoginActivity.this, "Error !"+ task.getException()  , Toast.LENGTH_SHORT ).show();
@@ -83,5 +87,10 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+
+
     }
+
+
+
 }
--- app/src/main/java/com/whatevervalue/parsingcsv/Meniu.java
+++ app/src/main/java/com/whatevervalue/parsingcsv/Meniu.java
@@ -40,7 +40,7 @@ public class Meniu extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.error_data_list, R.id.car_live_data)
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.error_data_list, R.id.car_live_data, R.id.loginActivity)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
